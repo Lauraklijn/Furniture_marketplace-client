abeb9506dd01f58a55acd9cc09d3ec4ef7e1ed35
Lauraklijn <lauraklijn12@hotmail.com>
2020-04-23 15:35:15 +0200

    Added cloudinary photo upload to homepageEdit

--- src/pages/MyHomepage/MyHomepageForm.js
+++ src/pages/MyHomepage/MyHomepageForm.js
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 import { useSelector, useDispatch } from "react-redux";
 import { selectMyHomepage } from "../../store/user/selectors";
 import { updateMyPage } from "../../store/user/actions";
+import AddImage from "../../components/Images/AddImage";
 
 export default function MyHomepageForm() {
   const homepage = useSelector(selectMyHomepage);
@@ -20,6 +21,20 @@ export default function MyHomepageForm() {
 
     dispatch(updateMyPage(title, description, imageUrl));
   }
+
+  const handleImageState = event => {
+    console.log("WHAT IS ImgURL in homepage?", event);
+
+    setimageUrl(event);
+    console.log("what is handleimage in homepage", handleImageState);
+  };
+
+  const handleInputs = event => {
+    setimageUrl({
+      [event.target.name]: event.target.value
+    });
+  };
+
   return (
     <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
       <h1 className="mt-5 mb-5">Edit your page</h1>
@@ -42,7 +57,7 @@ export default function MyHomepageForm() {
           type="text"
           placeholder="Description about the product"
         />
-      </Form.Group>
+        {/* </Form.Group>
       <Form.Group>
         <Form.Label>Image</Form.Label>
         <Form.Control
@@ -51,6 +66,26 @@ export default function MyHomepageForm() {
           type="tekst"
           placeholder="Add image"
         />
+      </Form.Group> */}
+      </Form.Group>
+      <Form.Label>Image url</Form.Label>
+      <Form.Group>
+        <AddImage
+          type="text"
+          name="image"
+          placeholder="Add image"
+          value={imageUrl}
+          onChange={handleInputs}
+          handleImageState={handleImageState}
+        />
+
+        {/* <Form.Control
+          value={imageUrl}
+          onChange={event => setImageUrl(event.target.value)}
+          type="text"
+          placeholder="Please add a picture of the product"
+          //handleImageState={this.handleImageState}
+        /> */}
       </Form.Group>
 
       <Form.Group className="mt-5">
